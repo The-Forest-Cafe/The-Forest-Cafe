61d375b3943b32b5fdd78898e58004fed5aeea57
Cha Seoyeon <seoyeon0833@gmail.com>
2025-09-18 11:51:48 +0900

    feat(system2): 구매창 UI 구현

---
+++ Assets/Scripts/System2/Purchase/PurchaseObjectInfo.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PurchaseObjectInfo : MonoBehaviour
+{
+    [SerializeField]
+    private PurchaseObjectSO objectSO;
+    [SerializeField]
+    private Image objectImage;
+    [SerializeField]
+    private Text objectName;
+    [SerializeField]
+    private Text objectDisc;
+    [SerializeField]
+    private Text objectPrice;
+    [SerializeField]
+    private SelectButton checkButton;
+
+    private void Start()
+    {
+        objectImage.sprite = objectSO.objectSprite;
+        objectName.text = $"{objectSO.objectName} {objectSO.quantity}개";
+        objectDisc.text = objectSO.objectDisc;
+        objectPrice.text = $"{objectSO.price.ToString()}원";
+        checkButton.onObjectSelected += SetObjectSelect;
+    }
+
+    // PurchaseObjectSO의 isSelected 변수의 값을 변경하는 함수
+    private void SetObjectSelect()
+    {
+        objectSO.isSelected = checkButton.GetIsSelected();
+
+        if (objectSO.isSelected == true)
+        {
+            TotalSum.Instance.totalSum += objectSO.price;
+        }
+        else
+        {
+            TotalSum.Instance.totalSum -= objectSO.price;
+        }
+
+        TotalSum.Instance.SetSumText();
+    }
+}
