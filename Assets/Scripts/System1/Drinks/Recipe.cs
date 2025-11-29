99c9c115b7a3707107056ccccd0f103cd76928af
zia0628 <zia0628@naver.com>
2025-11-29 22:58:39 +0900

    feat(system1):퀘스트 상호작용 기능 구현

--- Assets/Scripts/System1/Drinks/Recipe.cs
+++ Assets/Scripts/System1/Drinks/Recipe.cs
@@ -1,12 +1,25 @@
 using UnityEngine;
 using System.Collections.Generic;
 
+public enum DrinkTag
+{
+    None, 
+    Hot,  
+    Cold,     
+    Smoothie,   
+    BubbleTea,  
+    Latte       
+}
+
 [CreateAssetMenu(fileName = "New Recipe", menuName = "Cafe/Recipe")]
 public class Recipe : ScriptableObject
 {
     public string drinkName;            // 완성 음료 이름
     public Sprite drinkIcon;            // 완성 시 음료 이미지
     public GameObject drinkPrefab;      //3D 모델
+
+    [Header("음료 속성")]
+    public List<DrinkTag> drinkTags;
 
     public Ingredient baseIngredient;   // 필수 베이스 (예: "뜨거운 물")
     public Ingredient baseWay;
