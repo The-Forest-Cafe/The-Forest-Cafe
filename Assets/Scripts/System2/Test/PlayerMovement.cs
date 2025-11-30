de6e2e02aff925b902a2cc51ed24f5a1fb3776e0
Cha Seoyeon <seoyeon0833@gmail.com>
2025-11-30 21:15:36 +0900

    refactor(system2): 맵 콜라이더 값 수정

--- Assets/Scripts/System2/Test/PlayerMovement.cs
+++ Assets/Scripts/System2/Test/PlayerMovement.cs
@@ -5,14 +5,17 @@ public class PlayerMovement : MonoBehaviour
     private float h = 0.0f;
     private float v = 0.0f;
     private float r = 0.0f;
-    private Transform tr;
+    private Vector3 moveDir;
+
+    private Rigidbody rigid;
 
     public float moveSpeed = 10.0f;
     public float rotSpeed = 80.0f;
 
     private void Start()
     {
-        tr = transform;
+        rigid = GetComponent<Rigidbody>();
+        rigid.freezeRotation = true;
     }
 
     private void Update()
@@ -22,9 +25,12 @@ public class PlayerMovement : MonoBehaviour
         r = Input.GetAxis("Mouse X");
 
         // 전후좌우 이동 방향 벡터 계산
-        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+        moveDir = (Vector3.forward * v) + (Vector3.right * h);
+    }
 
-        tr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime, Space.Self);
-        tr.Rotate(Vector3.up * rotSpeed * Time.deltaTime * r);
+    private void FixedUpdate()
+    {
+        rigid.MovePosition(rigid.position + moveDir.normalized * moveSpeed * Time.fixedDeltaTime);
+        rigid.MoveRotation(rigid.rotation * Quaternion.Euler(Vector3.up * r * rotSpeed * Time.fixedDeltaTime));
     }
 }
