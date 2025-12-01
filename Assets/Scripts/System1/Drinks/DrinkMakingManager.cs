d445837f2ecf1652741320a8e803b20fd05d7a06
Cha Seoyeon <seoyeon0833@gmail.com>
2025-12-01 10:02:38 +0900

    fix(system2): 버그 수정

--- Assets/Scripts/System1/Drinks/DrinkMakingManager.cs
+++ Assets/Scripts/System1/Drinks/DrinkMakingManager.cs
@@ -168,6 +168,7 @@ public class DrinkMakingManager : MonoBehaviour
         currentDrink = result;
         Debug.Log($"음료 완성 및 저장: {currentDrink.drinkName}");
 
+        playerInventory.gameObject.GetComponent<PlayerInteraction>().RemoveDrinkModel();
         playerInventory.gameObject.GetComponent<PlayerInteraction>().hasDrink = true;
         playerInventory.gameObject.GetComponent<PlayerInteraction>().SetDrinkModel();
 
